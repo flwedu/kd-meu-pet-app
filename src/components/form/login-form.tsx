a78cb88867141f50a804e64ef8df108fc4df6018
Eduardo Aquino <edutraquino@gmail.com>
2022-06-08 16:14:47 -0300

    initial commit: remove code boilerplat and format login form

---
+++ src/components/form/login-form.tsx
@@ -0,0 +1,43 @@
+import { FormEvent, useState } from "react";
+
+export function LoginForm() {
+  const [data, setData] = useState({ username: "", password: "" });
+
+  function handleChange(e: FormEvent<HTMLInputElement>) {
+    const { name, value } = e.currentTarget;
+
+    setData({ ...data, [name]: value });
+  }
+
+  function handleSubmit() {
+    console.log("Submitted");
+  }
+
+  return (
+    <form
+      className="form form-login align-center flex flex-col rounded"
+      onSubmit={handleSubmit}
+    >
+      <div onChange={handleChange}>
+        <h2>Bem vindo!</h2>
+        <p>Por favor, se identifique:</p>
+        <div className="field">
+          <input
+            type="text"
+            name="username"
+            className="input"
+            placeholder="Usuário / E-mail"
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Senha"
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
