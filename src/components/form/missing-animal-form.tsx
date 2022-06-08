e7f051c05dfc1237372fa0806b117fdb4c14c339
Eduardo Aquino <edutraquino@gmail.com>
2022-06-08 20:58:32 -0300

    feat: font: ubuntu, add border to input fields

--- src/components/form/missing-animal-form.tsx
+++ src/components/form/missing-animal-form.tsx
@@ -7,7 +7,7 @@ export function MissingAnimalForm() {
     <div className="form rounded flex flex-col">
       <h2>Registrar animal desaparecido</h2>
       <form>
-        <div>
+        <div className="form-field">
           <span>Qual a espécie?</span>
           <div className="radio-group">
             <input
@@ -44,27 +44,37 @@ export function MissingAnimalForm() {
             ❓
           </div>
         </div>
-        <div>
-          <input
-            className="input"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nome (apelido) do animal"
-          />
+        <div className="form-field">
+          <span>Nome (apelido) do animal:</span>
+          <input className="input" type="text" name="name" id="name" />
         </div>
-        <div>
+        <div className="form-field">
+          <span>Descrição:</span>
           <textarea
             className="input"
             name="description"
             id="description"
-            placeholder="Descrição (em até 100 letras)"
+            placeholder="digite uma descrição em até 250 caracteres"
+            rows={5}
+            maxLength={250}
           ></textarea>
         </div>
-        <div>
-          <span>Anexe uma foto do desaparecido</span>
+        <div className="form-field">
+          <span>Sexo:</span>
+          <input type="radio" name="sex" value="male" /> M
+          <input type="radio" name="sex" value="female" /> F
+        </div>
+        <div className="form-field">
+          <span>Anexe uma foto do desaparecido:</span>
           <input type="file" name="picture" id="picture" />
         </div>
+
+        <div className="form-field">
+          <span>Última localização:</span>
+          <div style={{ height: "10rem", background: "green" }}></div>
+        </div>
+
+        <input type="submit" value="Cadastrar" />
       </form>
     </div>
   );
