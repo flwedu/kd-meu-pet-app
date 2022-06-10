1300b406093cfe0816d250310c9933fdd752546f
Eduardo Aquino <edutraquino@gmail.com>
2022-06-10 15:13:56 -0300

    chore: add types to api url and port

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAPS_KEY: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_API_PORT: string;
 }
 
 interface ImportMeta {
