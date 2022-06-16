ff760e8684fa15e72274966dd171766a440c251b
Eduardo Aquino <edutraquino@gmail.com>
2022-06-16 16:17:48 -0300

    refactoring: to class

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -1,18 +1,18 @@
-export const createApiClient = () => {
-  const HOST: string = import.meta.env.VITE_API_URL;
-  const PORT: string = import.meta.env.VITE_API_PORT;
-  const baseUrl: string = `http://${HOST}:${PORT}`;
+export class ApiClient {
+  private HOST: string = import.meta.env.VITE_API_URL;
+  private PORT: string = import.meta.env.VITE_API_PORT;
+  private baseUrl: string = `http://${this.HOST}:${this.PORT}`;
 
-  const get = async (url: string) => {
-    const response = await fetch(`${baseUrl}${url}`);
+  async get(url: string) {
+    const response = await fetch(`${this.baseUrl}${url}`);
     if (response.status !== 200) {
       return Promise.reject(response);
     }
     return await response.json();
-  };
+  }
 
-  const post = async (url: string, body: any) => {
-    const response = await fetch(`${baseUrl}${url}`, {
+  async post(url: string, body: any) {
+    const response = await fetch(`${this.baseUrl}${url}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -23,20 +23,20 @@ export const createApiClient = () => {
       return Promise.reject(response);
     }
     return await response.json();
-  };
+  }
 
-  const delete_ = async (url: string) => {
-    const response = await fetch(`${baseUrl}${url}`, {
+  async delete_(url: string) {
+    const response = await fetch(`${this.baseUrl}${url}`, {
       method: "DELETE",
     });
     if (response.status !== 204) {
       return Promise.reject(response);
     }
     return await response.json();
-  };
+  }
 
-  const put = async (url: string, body: any) => {
-    const response = await fetch(`${baseUrl}${url}`, {
+  async put(url: string, body: any) {
+    const response = await fetch(`${this.baseUrl}${url}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -47,7 +47,5 @@ export const createApiClient = () => {
       return Promise.reject(response);
     }
     return await response.json();
-  };
-
-  return { get, post, delete_, put };
-};
+  }
+}
