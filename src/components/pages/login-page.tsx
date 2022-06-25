16a0d98c161dea1411684e7896bc1f02c369b90f
Eduardo Aquino <edutraquino@gmail.com>
2022-06-25 15:32:38 -0300

    feat: base logic to login page

--- src/components/pages/login-page.tsx
+++ src/components/pages/login-page.tsx
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../hooks/contexts/auth-context";
+import { User } from "../../model/user";
+import { ApiClient } from "../../service/api-client";
 import { LoginForm, LoginFormData } from "../form/login-form";
 
 export function LoginPage() {
-  function onSubmit(data: LoginFormData) {}
+  const client = new ApiClient();
+  const { signIn, signOut } = useAuthContext();
+  const navigate = useNavigate();
+
+  function onSubmit(data: LoginFormData) {
+    console.log(data);
+
+    navigate("/home");
+
+    client
+      .post("/login", data)
+      .then((result) => {
+        const user = result.data as User;
+        signIn(user);
+
+        navigate("/home");
+      })
+      .catch(() => signOut());
+  }
 
   return <LoginForm onSubmit={onSubmit} />;
 }
